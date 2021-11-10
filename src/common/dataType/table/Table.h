b14d60dc167bceb63cec0b43ce1eb9f9616da3f3
liupeng <liupeng.0@outlook.com>
2021-11-11 01:04:30 +0800

    add: add src/common/dataType/table/Table.h

--- src/common/dataType/table/Table.h
+++ src/common/dataType/table/Table.h
@@ -1,12 +1,54 @@
 #ifndef _COMMON_DATATYPE_TABLE_
 #define _COMMON_DATATYPE_TABLE_
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <common/dataType/data/Data.h>
+
 namespace Common {
 namespace DataType {
 
-// class Table {
 
-// };
+class Table {
+
+public:
+
+    Table();
+
+    ~Table();
+
+    int getTableHight() const;
+
+    int getTablewith() const;
+
+    int setData(int row, int column, const Data& item);
+
+    Data getData(int row, int column) const;
+
+    std::vector<Data> getRow(int rowNum) const;
+
+    std::vector<std::string> getRowAsString(int rowNum) const;
+
+    std::vector<std::string> getColumn(int columnNum) const;
+
+    std::vector<Data> getColumnAsString(int columnNum) const;
+
+    std::vector<std::vector<std::string>> GetTableAsString() const;
+
+private:
+
+    Data *item;
+
+    Data *previousRowItem;
+    
+    Data *nextRowItem;
+
+    Data *previousColumnItem;
+
+    Data *nextColumnItem;
+
+};
 
 }
 }
