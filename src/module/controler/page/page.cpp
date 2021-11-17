923b3b88f3aae11e56db3400bf98e686a656b9fb
liupeng <liupeng.0@outlook.com>
2021-11-18 00:08:02 +0800

    add: add module create page

---
+++ src/module/controler/page/page.cpp
@@ -0,0 +1,18 @@
+#include "page.h"
+
+#include <common/timeType/dateTime/dateTime.h>
+
+using Common::TimeType::DateTime;
+using Common::DataType::ItemType;
+
+Common::DataType::Item Module::Controler::Page::CreatePage() {
+    Item item = Item();
+    item.setItemType(ItemType::PAGE);
+    item.setName("");
+    item.setId("");
+    DateTime *dateTime = new DateTime();
+    dateTime->init();
+    item.setDate_time(dateTime->getDataeTimeAsString());
+    item.setVersion("100");
+    return item;
+}
---
+++ src/module/controler/page/page.h
@@ -0,0 +1,21 @@
+#ifndef _MODULE_CONTROLER_PAGE_H_
+#define _MODULE_CONTROLER_PAGE_H_
+
+#include <common/dataType/item/Item.h>
+
+namespace Module {
+namespace Controler {
+
+using Common::DataType::Item;
+
+class Page {
+    
+public:
+    Item CreatePage();
+    
+
+};
+
+}
+}
+#endif
