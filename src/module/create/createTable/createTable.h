15b5bf60f3def0d03390544595e18f5a5b2cd695
liupeng <liupeng.0@outlook.com>
2021-11-26 23:52:06 +0800

    add: add create table module

---
+++ src/module/create/createTable/createTable.h
@@ -0,0 +1,27 @@
+#ifndef _MODULE_CREATE_CREATETABLE_H_
+#define _MODULE_CREATE_CREATETABLE_H_
+
+#include <iostream>
+
+
+namespace Module {
+namespace CreateTable {
+
+class CreateTable {
+
+public:
+    CreateTable();
+
+    ~CreateTable();
+
+private:
+
+    std::string info;
+
+    std:
+}
+
+} // namespace CreateTable
+} // namespace Module
+
+#endif
